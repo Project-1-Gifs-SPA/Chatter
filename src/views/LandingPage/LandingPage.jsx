8a78ac9c7333dca1b6bf883058891af3257aafdd
banagreg <bogdanovagergana97@gmail.com>
2023-11-21 20:38:06 +0200

    Changed the text in LandingPage

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -15,7 +15,7 @@ const LandingPage = () => {
 		<div className="hero min-h-screen bg-gray-900">
 			<div className="hero-content text-center">
 				<div className="max-w-md">
-					<h1 className="text-5xl font-bold text-purple-400" style={{ fontFamily: 'Rockwell, sans-serif' }}>Hello there!</h1>
+					<h1 className="text-5xl font-bold text-purple-400" style={{ fontFamily: 'Rockwell, sans-serif' }}>Welcome to Chatter!</h1>
 					<p className="py-6 text-gray-200">Join the conversation, meet new people, and build connections in a single spot!</p>
 					<Link to='/sign-in'>
 						<button style={{ fontFamily: 'Rockwell, sans-serif' }} className="btn btn-primary mr-5">Sign in</button>
