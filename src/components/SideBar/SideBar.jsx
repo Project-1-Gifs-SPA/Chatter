a6735a282d1467784d51de033bb4726cf6a932f3
banagreg <bogdanovagergana97@gmail.com>
2023-11-21 17:27:28 +0200

    Added Sign out button in SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import TeamIcon from '../TeamIcon/TeamIcon';
 import DMIcon from '../DMIcon/DMIcon';
 import AddTeams from '../AddTeams/AddTeams';
+import { logoutUser } from '../../services/auth.service';
+import AppContext from '../../context/AppContext';
+import { useNavigate } from 'react-router';
 
 const SideBar = () => {
+	const { user, userData, setContext } = useContext(AppContext);
+	const navigate = useNavigate();
+
+
+	const onLogout = () => {
+		logoutUser()
+			.then(() => {
+				setContext({
+					user: null,
+					userData: null,
+				});
+			});
+		navigate('/welcome')
+	};
 	return (
 		<>
-			<div className="bg-gray-900 text-purple-lighter flex-none w-24 p-6 hidden md:block ">
-				<DMIcon />
-				<TeamIcon />
-				<TeamIcon />
-				<TeamIcon />
-				<TeamIcon />
-				<AddTeams />
+			<div className="flex flex-col justify-between h-screen bg-gray-900">
+				<div className="bg-gray-900 text-purple-lighter flex-none w-24 p-6 hidden md:block">
+					{/* Your existing content */}
+					<DMIcon />
+					<TeamIcon />
+					<TeamIcon />
+					<TeamIcon />
+				</div>
+
+				<div className="bg-gray-900 text-purple-lighter flex-none w-24 p-6 hidden md:block">
+					{/* Button at the bottom */}
+					<button className="text-white px-4 py-2 rounded btn bg-purple-600 transition-colors hover:bg-purple-400 border-none" onClick={onLogout}>
+						Sign out
+					</button>
+				</div>
 			</div>
 		</>
 	)
