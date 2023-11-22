d481aa271633f3175ec8c4b23c34f84e14482393
banagreg <bogdanovagergana97@gmail.com>
2023-11-22 17:25:48 +0200

    Added default team photo

--- src/components/TeamIcon/TeamIcon.jsx
+++ src/components/TeamIcon/TeamIcon.jsx
@@ -3,41 +3,43 @@ import { getLiveTeamInfo } from '../../services/teams.service';
 import { set } from 'firebase/database';
 import { useNavigate } from 'react-router-dom';
 
-const TeamIcon = ({id}) => {
+const TeamIcon = ({ id }) => {
 
 
 	const [currentTeam, setCurrentTeam] = useState({});
 	const navigate = useNavigate();
 
 
-	useEffect(()=>{
+	useEffect(() => {
 		console.log('get team info')
 
-		const unsubscribe =	getLiveTeamInfo( data=> {
+		const unsubscribe = getLiveTeamInfo(data => {
 
-		setCurrentTeam({...data})
+			setCurrentTeam({ ...data })
 
 		}, id)
-		return ()=>{
+		return () => {
 			unsubscribe();
 		}
 
-	},[id])
-
-
-	
+	}, [id])
 
 	return (
-		<div className="cursor-pointer mb-4" onClick={()=> navigate(`/teams/${currentTeam.id}`)}>
-			<div className="tooltip" data-tip={currentTeam.name}>
-			<div
-				className="bg-white h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-3xl mb-1 overflow-hidden hover:rounded-md">
-				<img src="https://cdn.discordapp.com/embed/avatars/4.png" alt="" />
-				
-			</div>
+		<div className="cursor-pointer mb-4" onClick={() => navigate(`/teams/${currentTeam.id}`)}>
+			<div className="tooltip tooltip-right" data-tip={currentTeam.name}>
+				<div
+					className="bg-orange-500 h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-3xl mb-1 overflow-hidden hover:rounded-md">
+					{currentTeam?.name && (
+						<span className='text-xl'>
+							{currentTeam.name.includes(' ')
+								? currentTeam.name.split(' ').map(name => name[0]).join('')
+								: currentTeam.name.substring(0, 1)}
+						</span>
+					)}
+				</div>
 
-			</div>	
-		</div>
+			</div>
+		</div >
 	)
 }
 
