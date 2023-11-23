04d8bc7f4b79dea51afe46ce0d3e6f2070e1ee86
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-11-23 23:33:00 +0200

    Added ChannelTile.jsx

--- src/components/ChannelTile/ChannelTile.jsx
+++ src/components/ChannelTile/ChannelTile.jsx
@@ -1,10 +1,31 @@
-const ChannelTitle = (channelName) => {
+import { useEffect, useState } from "react";
+import { getChannelById } from "../../services/channel.service";
+import { useNavigate, useParams } from "react-router";
+
+const ChannelTile = () => {
+
+    const { teamId, channelId } = useParams();
+
+    const [channelName, setChannelName] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log('channel id ==========================================');
+        console.log(channelId)
+        getChannelById(channelId)
+            .then(channel => {
+                setChannelName(channel.name);
+            })
+    }, [channelId])
 
     return (
-        <div>
-            {channelName}
-        </div>
+        <button
+            onClick={() => {navigate(`/teams/${teamId}/channels/${channelId}`)}}
+        >
+            { channelName }
+        </button >
     )
 }
 
-export default ChannelTitle;
+export default ChannelTile;
