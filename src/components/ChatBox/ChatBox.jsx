9504ac5c51e473c8bc1e798aeb65724e4d43ee12
Aleksandar Spasov <alekspasov@gmail.com>
2023-11-23 18:42:03 +0200

    Added scrollToBottom fun in ChatBox.jsx

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,104 +1,112 @@
-import React, { useState, useRef, useEffect, useContext } from 'react'
-import Message from '../Message/Message'
-import ChatTopBar from '../ChatTopBar/ChatTopBar'
-import bg from '../../assets/background.png'
-import { set } from 'firebase/database'
-import { getChatTest, getLiveMessages, getLiveMessagesTest, sendMessageTest } from '../../services/chat.service'
-import { useParams } from 'react-router'
-import AppContext from '../../context/AppContext'
+import React, { useState, useRef, useEffect, useContext } from "react";
+import Message from "../Message/Message";
+import ChatTopBar from "../ChatTopBar/ChatTopBar";
+import bg from "../../assets/background.png";
+import { set } from "firebase/database";
+import {
+  getChatTest,
+  getLiveMessages,
+  getLiveMessagesTest,
+  sendMessageTest,
+} from "../../services/chat.service";
+import { useParams } from "react-router";
+import AppContext from "../../context/AppContext";
 
 const ChatBox = () => {
+  // const messagesEndRef = useRef();
+  const { teamId } = useParams();
+  const { user, userData } = useContext(AppContext);
 
-	// const messagesEndRef = useRef();
-	const{teamId} = useParams();
-	const{user, userData} = useContext(AppContext);
+  // const scrollToBottom = () => {
+  // 	messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  // }
 
-	// const scrollToBottom = () => {
-	// 	messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-	// }
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState([]);
 
+  const scrollToBottom = () => {
+    const chat = document.getElementById("chat");
+    chat.scrollTop = chat.scrollHeight;
+  };
 
-	const[msg, setMsg] = useState('');
-	const[messages, setMessages] = useState([])
+  const container = useRef(null);
 
+  const scroll = () => {
+    const { offsetHeight, scrollHeight, scrollTop } = container.current;
+    if (scrollHeight <= scrollTop + offsetHeight + 100) {
+      container.current?.scrollTo(0, scrollHeight);
+    }
+  };
 
-	const container = useRef(null)
+  useEffect(() => {
+    scroll();
+  }, [messages]);
 
-	const Scroll = () => {
-	  const { offsetHeight, scrollHeight, scrollTop } = container.current;
-	  if (scrollHeight <= scrollTop + offsetHeight + 100) {
-		container.current?.scrollTo(0, scrollHeight)
-	  }
-	}
-  
-	useEffect(() => {
-	  Scroll()
-	}, [messages])
+  // useEffect(()=> scrollToBottom, [messages]);
 
-	
+  useEffect(() => {
+    console.log("live msg");
+    getChatTest(teamId)
+      .then((chatArr) => setMessages(chatArr))
+      .then(() => scrollToBottom());
+  }, [teamId]);
 
+  useEffect(() => {
+    console.log("live msg");
 
-	// useEffect(()=> scrollToBottom, [messages]);
+    const unsubscribe = getLiveMessagesTest((snapshot) => {
+      setMessages(Object.values(snapshot.val()));
+    }, teamId);
 
+    return () => unsubscribe;
+  }, [teamId]);
 
-	useEffect(()=> {
-		console.log('live msg')
-		getChatTest(teamId)
-		.then(chatArr=> setMessages(chatArr))
+  const handleMsg = (e) => {
+    e.preventDefault();
 
-	},[teamId])
+    sendMessageTest(teamId, userData.handle, msg, userData.photoURL).then(() =>
+      setMsg("")
+    );
+  };
 
-	useEffect(()=> {
-		console.log('live msg')
-
-		const unsubscribe = getLiveMessagesTest((snapshot=>{
-
-			setMessages(Object.values(snapshot.val()));
-		
-		}),teamId)
-
-		return () => unsubscribe;
-
-	},[teamId])
-
-
-
-
-	const handleMsg =(e) =>{
-		e.preventDefault();
-
-		sendMessageTest(teamId, userData.handle, msg, userData.photoURL )
-		.then(()=> setMsg(''));
-	
-		
-	}
-
-	return (
-		<div className="flex-1 flex flex-col bg-gray-700 overflow-hidden"
-		>
-			{/* Top bar */}
-			<ChatTopBar />
-			{/* <!-- Chat messages --> */}
-			<div ref={container} className="px-6 py-4 flex-1 overflow-y-scroll">
-				{/* <Message />
+  return (
+    <div className="flex-1 flex flex-col bg-gray-700 overflow-hidden">
+      {/* Top bar */}
+      <ChatTopBar />
+      {/* <!-- Chat messages --> */}
+      <div
+        ref={container}
+        className="px-6 py-4 flex-1 overflow-y-scroll"
+        id="chat"
+      >
+        {/* <Message />
 				<Message /> */}
-				{messages.length ? messages.map(message=> <Message key={message.id} message={message} />) : null}
-			</div>
-			<form 
-			style={{backgroundColor: "black", color: "white", fontWeight: "bold", border: "none", padding: "10px 20px"}} 
-			onSubmit={handleMsg}>
-				<input 
-				style={{padding:"10px 20px", width:"100%"}}
-				type="text" 
-				value={msg}
-				
-				onChange={(e)=>setMsg(e.target.value)}
-			
-				/>
-				{/* <button type='submit' className='ml-50'>Send</button> */}
-				</form>
-		</div>
-	)
-}
+        {messages.length
+          ? messages.map((message) => (
+              <Message key={message.id} message={message} />
+            ))
+          : null}
+      </div>
+      <form
+        style={{
+          backgroundColor: "gray 900",
+          color: "white",
+          fontWeight: "bold",
+          border: "none",
+          padding: "10px 20px",
+        }}
+        onSubmit={handleMsg}
+      >
+        <input
+          style={{ padding: "10px 20px", width: "100%" }}
+          type="text"
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+        />
+        {/* <button type='submit' className='ml-50'>Send</button> */}
+      </form>
+    </div>
+  );
+};
 
-export default ChatBox
+export default ChatBox;
