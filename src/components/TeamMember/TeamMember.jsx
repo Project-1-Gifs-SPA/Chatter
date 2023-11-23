eaf34072674940ef08b8f4a05949d49d261ec0f7
banagreg <bogdanovagergana97@gmail.com>
2023-11-24 00:41:43 +0200

    Added other user's profile view

--- src/components/TeamMember/TeamMember.jsx
+++ src/components/TeamMember/TeamMember.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProfileModal from '../ProfileModal/ProfileModal';
 
 const TeamMember = ({ member, owner }) => {
+	const [showModal, setShowModal] = useState(false);
+
 	return (
-		<div className="flex p-3 relative">
-			{member.handle === owner && (<p p className="absolute top-2 left-8 transform -translate-x-1/2 -translate-y-1/2">👑</p>)}
-			<div className={`avatar ${member.availability} relative`}>
-				<div className="w-10 rounded-full">
-					<img src={member.photoURL} alt="User Avatar" />
+		<>
+			<div className="flex p-3 relative">
+				{member.handle === owner && (<p p className="absolute top-2 left-8 transform -translate-x-1/2 -translate-y-1/2">👑</p>)}
+				<div className={`avatar ${member.availability} relative z-[0]`}>
+
+					<div className="w-10 rounded-full">
+						<img src={member.photoURL} alt="User Avatar"
+							onClick={() => setShowModal(true)} />
+					</div>
 				</div>
-			</div>
-			<div className="leading-4 pl-3 text-white">
-				<h4 className="font-semibold">{member.firstName} {member.lastName}</h4>
-				<span className="text-xs text-white">{member.handle}</span>
-			</div>
-		</div >
+				<div className="leading-4 pl-3 text-white">
+					<h4 className="font-semibold">{member.firstName} {member.lastName}</h4>
+					<span className="text-xs text-white">{member.handle}</span>
+				</div>
+			</div >
+			{showModal && <ProfileModal isVisible={showModal} onClose={() => setShowModal(false)} publicProfile={member} />}
+		</>
 	)
 }
 
