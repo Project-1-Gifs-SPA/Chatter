0fb1a9707660e0894a51ec4436f880a4564326c4
banagreg <bogdanovagergana97@gmail.com>
2023-11-26 15:40:46 +0200

    Added Emoji picker to chat box

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -14,6 +14,10 @@ import {
 } from "../../services/chat.service";
 import { useParams } from "react-router";
 import AppContext from "../../context/AppContext";
+import { FaRegSmile } from "react-icons/fa";
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data'
+
 
 const ChatBox = () => {
 	// const messagesEndRef = useRef();
@@ -25,7 +29,7 @@ const ChatBox = () => {
 	// }
 
 	// useEffect(()=> scrollToBottom, [messages]);
-
+	const [isPickerVisible, setPickerVisible] = useState(false);
 	const [msg, setMsg] = useState("");
 	const [messages, setMessages] = useState([]);
 
@@ -43,6 +47,20 @@ const ChatBox = () => {
 		}
 	};
 
+
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	const handleWindowSizeChange = () => {
+		setWindowWidth(window.innerWidth);
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', handleWindowSizeChange);
+		return () => {
+			window.removeEventListener('resize', handleWindowSizeChange);
+		};
+	}, []);
+
 	useEffect(() => {
 		scroll();
 	}, [messages]);
@@ -51,53 +69,54 @@ const ChatBox = () => {
 
 	useEffect(() => {
 		console.log("live msg");
-		if(channelId){
-		getChat(channelId)
-			.then((response) => setMessages(Object.values(response)))
-			.then(() => scrollToBottom());
+		if (channelId) {
+			getChat(channelId)
+				.then((response) => setMessages(Object.values(response)))
+				.then(() => scrollToBottom());
 		}
-		if(dmId){
+		if (dmId) {
 			getDMChat(dmId)
-				.then((response)=> setMessages(Object.values(response)))
-				.then(()=>scrollToBottom())
+				.then((response) => setMessages(Object.values(response)))
+				.then(() => scrollToBottom())
 		}
-	}, [channelId,dmId]);
+	}, [channelId, dmId]);
 
 	useEffect(() => {
 		console.log("live msg");
 
 
-		if(channelId){
+		if (channelId) {
 			const unsubscribe = getLiveMessages((snapshot) => {
 				setMessages(Object.values(snapshot.val()));
 			}, channelId);
-	
+
 			return () => unsubscribe;
 		}
-		if(dmId){
-			const unsubscribe = getLiveDirectMessages((snapshot)=>{
+		if (dmId) {
+			const unsubscribe = getLiveDirectMessages((snapshot) => {
 				setMessages(Object.values(snapshot.val()));
 			}, dmId);
 
 			return () => unsubscribe;
 		}
 
-	
+
 	}, [channelId, dmId]);
 
 	const handleMsg = (e) => {
 		e.preventDefault();
 
-		if(channelId){
+		if (channelId) {
 			sendMessage(channelId, userData.handle, msg, userData.photoURL)
-			.then(() => setMsg(""));
+				.then(() => setMsg(""));
 		}
 
-		if(dmId){
+		if (dmId) {
 			sendDirectMessage(dmId, userData.handle, msg, userData.photoURL)
-			.then(()=> setMsg(''));
-		}	
+				.then(() => setMsg(''));
+		}
 	};
+	console.log(isPickerVisible)
 
 	return (
 		<div className="flex-1 flex flex-col bg-gray-700">
@@ -117,24 +136,54 @@ const ChatBox = () => {
 					))
 					: null}
 			</div>
-			{channelId || dmId ? <form
-				style={{
-					backgroundColor: "gray 900",
-					color: "white",
-					fontWeight: "bold",
-					border: "none",
-					padding: "10px 20px",
-				}}
-				onSubmit={handleMsg}
-			>
-				<input
-					style={{ padding: "10px 20px", width: "100%" }}
-					type="text"
-					value={msg}
-					onChange={(e) => setMsg(e.target.value)}
-				/>
-				{/* <button type='submit' className='ml-50'>Send</button> */}
-			</form> : null}
+
+			<div className='flex items-center' style={{ width: "100%", outline: 'none' }}>
+				<div className='flex-grow'>
+					{channelId || dmId ? <form
+						style={{
+							backgroundColor: "gray 900",
+							color: "white",
+							border: "none",
+							padding: "10px 20px",
+						}}
+						onSubmit={handleMsg}
+					>
+						<input
+							className="bg-gray-800 border-none rounded"
+							style={{ padding: "10px 20px", width: "100%", outline: 'none' }}
+							type="text"
+							value={msg}
+							onChange={(e) => setMsg(e.target.value)}
+						/>
+
+						{/* <button type='submit' className='ml-50'>Send</button> */}
+					</form> : null}
+				</div>
+				<div className="relative inline-block pr-5">
+					<div className={`absolute z-10 ${isPickerVisible ? '' : 'hidden'} mt-2`}
+						style={{
+							bottom: '30px',
+							left: 'auto',
+							right: '0'
+						}}>
+						<Picker
+							data={data} previewPosition='none' onEmojiSelect={(e) => {
+								setPickerVisible(!isPickerVisible);
+								setMsg(msg + e.native);
+							}} />
+					</div>
+					<button style={{
+						//transform: 'translateY(-50%)',
+						background: 'transparent',
+						border: 'none',
+						outline: 'none',
+						cursor: 'pointer',
+						color: 'white',
+					}} className='btn btn-xs rounded-full w-8 h-8' onClick={() => setPickerVisible(!isPickerVisible)}>
+						<FaRegSmile className="w-6 h-6" />
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
