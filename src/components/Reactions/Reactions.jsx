36613ba90b1bd0abdcd93c393115bcb729538c80
banagreg <bogdanovagergana97@gmail.com>
2023-11-28 18:19:44 +0200

    change: Removed repeated code

--- src/components/Reactions/Reactions.jsx
+++ src/components/Reactions/Reactions.jsx
@@ -8,9 +8,10 @@ import { addDmReaction, removeDMReaction } from '../../services/dms.service';
 const Reactions = ({ msg, reaction, count }) => {
 	const { userData } = useContext(AppContext);
 	const { channelId, dmId } = useParams();
+	const hasUserReacted = Object.keys(msg.reactions[reaction]).includes(userData.handle);
 
 	const handleClick = () => {
-		if (Object.keys(msg.reactions[reaction]).includes(userData.handle)) {
+		if (hasUserReacted) {
 			if (channelId) {
 				removeChannelReaction(reaction, userData.handle, channelId, msg.id);
 			}
@@ -27,7 +28,7 @@ const Reactions = ({ msg, reaction, count }) => {
 		}
 	}
 	return (
-		<div className={`badge m-1 bg-gray-600 p-1 w-9 flex border-gray-700 cursor-pointer ${Object.keys(msg.reactions[reaction]).includes(userData.handle) && 'border-2 border-indigo-600'}`} onClick={handleClick}>
+		<div className={`badge m-1 bg-gray-600 p-1 w-9 flex border-gray-700 cursor-pointer ${hasUserReacted && 'border-2 border-indigo-600'}`} onClick={handleClick}>
 			<p className='ml-1'>{reactions[reaction]}</p>
 			<p className='text-gray-300 ml-1 mr-1'>{count}</p>
 		</div>
