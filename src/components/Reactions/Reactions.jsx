5a53f18d99d5ed78afc63caae841bda12de24ff4
banagreg <bogdanovagergana97@gmail.com>
2023-11-28 18:09:50 +0200

    Changed border color for reaction badge

--- src/components/Reactions/Reactions.jsx
+++ src/components/Reactions/Reactions.jsx
@@ -27,7 +27,7 @@ const Reactions = ({ msg, reaction, count }) => {
 		}
 	}
 	return (
-		<div className={`badge m-1 bg-gray-600 p-1 w-9 flex cursor-pointer ${Object.keys(msg.reactions[reaction]).includes(userData.handle) && 'border-2 border-indigo-600'}`} onClick={handleClick}>
+		<div className={`badge m-1 bg-gray-600 p-1 w-9 flex border-gray-700 cursor-pointer ${Object.keys(msg.reactions[reaction]).includes(userData.handle) && 'border-2 border-indigo-600'}`} onClick={handleClick}>
 			<p className='ml-1'>{reactions[reaction]}</p>
 			<p className='text-gray-300 ml-1 mr-1'>{count}</p>
 		</div>
