a460fbcec014858ae28f1d4c38f25632bbd5fe38
banagreg <bogdanovagergana97@gmail.com>
2023-11-29 11:48:34 +0200

    fix: context menu position

--- src/components/TeamIcon/TeamIcon.jsx
+++ src/components/TeamIcon/TeamIcon.jsx
@@ -9,18 +9,18 @@ import EditTeamModal from '../EditTeamModal/EditTeamModal';
 
 const TeamIcon = ({ id }) => {
 
-	const{userData} = useContext(AppContext)
+	const { userData } = useContext(AppContext)
 	const [currentTeam, setCurrentTeam] = useState({});
 	const [contextMenuVisible, setContextMenuVisible] = useState(false);
-	const[showModal, setShowModal] = useState(false);
-	
-	const navigate = useNavigate();
-	
+	const [showModal, setShowModal] = useState(false);
 
-	const handleContextMenu =(e)=>{	
+	const navigate = useNavigate();
+
+
+	const handleContextMenu = (e) => {
 		e.preventDefault();
 		setContextMenuVisible(true);
-}
+	}
 
 	useEffect(() => {
 		console.log('get team info')
@@ -38,33 +38,35 @@ const TeamIcon = ({ id }) => {
 
 	return (
 		<>
-		<div className="cursor-pointer mb-4" onClick={() =>
-			getGeneralChannel(currentTeam.id)
-				.then(channelId => navigate(`/teams/${currentTeam.id}/channels/${channelId}`))}
+			<div className="cursor-pointer mb-4" onClick={() =>
+				getGeneralChannel(currentTeam.id)
+					.then(channelId => navigate(`/teams/${currentTeam.id}/channels/${channelId}`))}
 				onContextMenu={handleContextMenu}
-		>
-			
-			<div className="tooltip tooltip-right" data-tip={currentTeam.name}>
-				<div
-					className="bg-orange-500 h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-3xl mb-1 overflow-hidden hover:rounded-md">
-					{currentTeam.photoURL?
-					
-					<img src={currentTeam.photoURL} />
-					: currentTeam?.name && (
-						<span className='text-xl'>
-							{currentTeam.name.includes(' ')
-								? currentTeam.name.split(' ').map(name => name[0]).join('')
-								: currentTeam.name.substring(0, 1)}
-						</span>
-					)}
+			>
+
+				<div className="tooltip tooltip-right" data-tip={currentTeam.name}>
+					<div
+						className="bg-orange-500 h-12 w-12 flex items-center justify-center text-black text-2xl font-semibold rounded-3xl mb-1 overflow-hidden hover:rounded-md">
+						{currentTeam.photoURL ?
+
+							<img src={currentTeam.photoURL} />
+							: currentTeam?.name && (
+								<span className='text-xl'>
+									{currentTeam.name.includes(' ')
+										? currentTeam.name.split(' ').map(name => name[0]).join('')
+										: currentTeam.name.substring(0, 1)}
+								</span>
+							)}
+					</div>
+
 				</div>
-				
-			</div>
-			
-		</div >
-		{contextMenuVisible &&  <ContextMenu teamId={id} owner={currentTeam.owner} contextMenuVisible={contextMenuVisible} setContextMenuVisible={setContextMenuVisible} 
-		showModal={showModal} setShowModal={setShowModal}/>}
-		{showModal ?<EditTeamModal teamId={id} name={currentTeam.name} onClose={()=>setShowModal(false)} teamPic={currentTeam.photoURL}/> : null}
+				{contextMenuVisible && <div className='absolute top-auto left-auto z-[1000]'>
+					<ContextMenu teamId={id} owner={currentTeam.owner} contextMenuVisible={contextMenuVisible} setContextMenuVisible={setContextMenuVisible}
+						showModal={showModal} setShowModal={setShowModal} />
+				</div>}
+			</div >
+
+			{showModal ? <EditTeamModal teamId={id} name={currentTeam.name} onClose={() => setShowModal(false)} teamPic={currentTeam.photoURL} /> : null}
 		</>
 	)
 }
