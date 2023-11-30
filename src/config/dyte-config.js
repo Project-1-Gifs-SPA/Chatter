b7bf3eae14770916e3e23ed1010f6fb8f9e8d6af
Aleksandar Spasov <alekspasov@gmail.com>
2023-11-30 15:39:55 +0200

    Added meetings service fns

---
+++ src/config/dyte-config.js
@@ -0,0 +1,6 @@
+
+export const DYTE_API_KEY = 'a985e063e57ec3298dd9';
+
+export const DYTE_ORG_KEY = '01d129a8-57a9-4b0e-8389-f639ad7b3a1a';
+
+export const DYTE_BASE_URL = 'https://api.dyte.io/v2';
---
+++ src/services/meetings.service.js
@@ -0,0 +1,113 @@
+import { ref, push, get, set, query, equalTo, orderByChild, update, endAt, startAt, onValue } from 'firebase/database';
+import { db } from '../config/firebase-config';
+
+
+export const createMeeting = (handle, participants, topic, start,end, ) => {
+    return push(ref(db, 'meetings'),{})
+    .then(response => {
+        
+        const members={};
+        participants.map(participant=> members[participant]=true)
+        
+        set(ref(db,`meetings/${response.key}`),
+            {
+                topic: topic,
+                members,
+                id:response.key,
+                owner: handle,
+                start,
+                end,
+            });
+        return update(ref(db),{
+            [`users/${handle}/meetings/${response.key}`]: true,
+        })
+        .then(()=> response.key);
+
+    });
+};
+
+export const getMeetingById = (meetingId) => {
+    return get(ref(db, `meetings/${meetingId}`))
+    .then(snapshot => snapshot.exists() ? snapshot.val() : {});
+}
+
+export const addMemberToMeeting = (meetingId, handle) =>{
+    const addMember = {};
+    addMember[`meetings/${meetingId}/members/${handle}`] = true;
+    addMember[`users/${handle}/meetings/${meetingId}`] = true;
+
+    return update(ref(db), addMember);
+}
+
+
+export const addMeetingRoomId = (meetingId, roomId) => {
+    const updateRoom  = {};
+    updateRoom[`meetings/${meetingId}/room`] = roomId;
+
+    return update(ref(db), updateRoom);
+}
+
+export const fromMeetingsDocument = (snapshot) => {
+    const meetingsDocument =  snapshot.val();
+
+
+    return Object.key(meetingsDocument).map(key=>{
+        const meeting =meetingsDocument[key];
+
+        return{
+            ...meeting,
+            id: key,
+            members: meeting.members? Object.keys(meeting.members) : [];
+        }
+
+    })
+
+}
+
+export const getAllMeetings = () => {
+    return get(ref(db,'meetings'))
+    .then((snapshot)=>{
+    const data = snapshot.exists() ? snapshot : [];
+
+    return fromMeetingsDocument(data);
+});
+}
+
+export const getMeetingsByHandle = (handle) => {
+    
+}
+
+
+export const getAllMeetingsByHandle = (handle) => {
+    return get(ref(db,`users/${handle}/meetings`))
+    .then(snapshot=>{
+        const data = snapshot.exists() ? Object.keys(snapshot.val()) : [];
+
+        return data;
+    })
+}
+
+
+
+export const getLiveAllMeetings = (listenFn) => {
+    return onValue(
+        ref(db, 'meetings'),
+        snapshot => {
+            const data = snapshot.exists() ? Object.values(snapshot.val()) : {};
+            listenFn(data);
+        }
+    )
+}
+
+
+export const getLiveMeetingMembers = (listenFn, meetingId) => {
+    return onValue(
+        ref(db, `meetings/${meetingId}/members`),
+        snapshot => {
+            const data = snapshot.exists() ? Object.keys(snapshot.val()) : [];
+
+            listenFn(data);
+        }
+    )
+}
+
