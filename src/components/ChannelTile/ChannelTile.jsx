501213199900fbf340e7e8cad4e413d67d6a4cf1
PH4NT0MK <ph4nt0mk1llr@gmail.com>
2023-12-01 11:05:52 +0200

    Testing to fix bug

--- src/components/ChannelTile/ChannelTile.jsx
+++ src/components/ChannelTile/ChannelTile.jsx
@@ -3,44 +3,52 @@ import { getChannelById, getGeneralChannel, removeChannel } from "../../services
 import { useNavigate, useParams } from "react-router";
 import { AiOutlineClose } from "react-icons/ai";
 
-const ChannelTile = ({ channelId, generalId }) => {
+const ChannelTile = ({ channelTileId, generalId }) => {
 
     const { teamId } = useParams();
 
     const [channelName, setChannelName] = useState('');
     const [channelIdKept, setChannelIdKept] = useState('');
 
+    const handleRemoveChannel = (id) => (teamId) => {
+        removeChannel(teamId, id);
+    }
+
     const navigate = useNavigate();
 
     useEffect(() => {
-        setChannelIdKept(channelId);
-        getChannelById(channelId)
+        // setChannelIdKept(handleRemoveChannel(channelTileId));
+        console.log(`channelTileId ${channelTileId}`);
+        setChannelIdKept(channelTileId);
+        console.log(`channelIdKept ${channelIdKept}`);
+        getChannelById(channelTileId)
             .then(channel => {
                 setChannelName(channel.name);
             });
-    }, [channelId]);
+    }, [channelTileId]);
 
     return (
         <div className="items-center ml-auto mr-auto display-flex">
             <button
-                onClick={() => { console.log(channelId); navigate(`/teams/${teamId}/channels/${channelId}`); }}
+                onClick={() => { console.log(channelTileId); navigate(`/teams/${teamId}/channels/${channelTileId}`); }}
             >
                 {channelName}
             </button >
-            {channelId !== generalId && <button
-                onClick={() => {document.getElementById("delete-channel").showModal()}}
+            {channelTileId !== generalId && <button
+                onClick={() => { document.getElementById("delete-channel").showModal() }}
             >
                 <AiOutlineClose />
-                <dialog  id="delete-channel" className="modal">
+                <dialog id="delete-channel" className="modal">
                     <div className="modal-box">
                         <h3 className="font-bold text-lg">Are you sure you want to remove this channel?</h3>
-                        <span className="bg-red">{}</span>
+                        <span className="bg-red">{ }</span>
 
                         <div className="modal-action">
 
                             <form method="dialog" >
                                 <button className="btn mr-5" >Close</button>
-                                <button className="btn bg-red-700" onClick={() => { console.log(channelIdKept); /*removeChannel(teamId, channelId);*/ }}>Remove channel</button>
+                                <button className="btn bg-red-700"
+                                    onClick={(id) => { console.log(id); console.log(channelTileId); console.log(channelIdKept); /*removeChannel(teamId, channelTileId);*/ }}>Remove channel</button>
                             </form>
 
                         </div>
