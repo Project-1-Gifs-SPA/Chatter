af7b17767af0027373ea56aa84f3b7eab12cbc86
banagreg <bogdanovagergana97@gmail.com>
2023-12-01 17:33:23 +0200

    Added isChannelSeen in ChannelTitle

--- src/components/ChannelTile/ChannelTile.jsx
+++ src/components/ChannelTile/ChannelTile.jsx
@@ -1,31 +1,55 @@
-import { useEffect, useState } from "react";
-import { getChannelById } from "../../services/channel.service";
+import { useContext, useEffect, useState } from "react";
+import { getChannelById, getLiveChannelSeenBy } from "../../services/channel.service";
 import { useNavigate, useParams } from "react-router";
+import AppContext from "../../context/AppContext";
+import { setChannelSeenBy, setTeamSeenBy, setTeamsNotSeenBy } from "../../services/chat.service";
 
 const ChannelTile = ({ channelId }) => {
 
     const { teamId } = useParams();
+    const { userData } = useContext(AppContext);
 
     const [channelName, setChannelName] = useState('');
+    const [currentChannel, setCurrentChannel] = useState({})
+    const [isChannelSeen, setIsChannelSeen] = useState([])
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        console.log('channel id ==========================================');
-        console.log(channelId)
         getChannelById(channelId)
             .then(channel => {
-                console.table(channel);
                 setChannelName(channel.name);
+                setCurrentChannel(channel)
             })
     }, [channelId])
 
+    useEffect(() => {
+        const unsubscribe = getLiveChannelSeenBy(data => {
+            setIsChannelSeen([...data])
+        }, channelId)
+        return () => {
+            unsubscribe();
+        }
+    }, [channelId])
+
+    console.log('channel ', channelId, ' ', isChannelSeen)
+
+    useEffect(() => {
+        if (!isChannelSeen.includes(userData.handle)) {
+            setTeamsNotSeenBy(teamId, userData.handle);
+        } else {
+            setTeamSeenBy(teamId, userData.handle)
+        }
+    }, [isChannelSeen])
+
     return (
-        <button className="text-white ml-5"
-            onClick={() => { navigate(`/teams/${teamId}/channels/${channelId}`) }}
-        >
-            {channelName}
-        </button >
+        <div>
+            <button className={`${isChannelSeen.includes(userData.handle) ? 'text-white' : 'text-red-400'} ml-4`}
+                onClick={() => { navigate(`/teams/${teamId}/channels/${channelId}`) }}
+            >
+                {channelName}
+            </button >
+        </div>
     )
 }
 
