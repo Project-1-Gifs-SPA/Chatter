2b4eae17c9ab54ce28d71111690432d126ec46b4
banagreg <bogdanovagergana97@gmail.com>
2023-12-01 17:29:22 +0200

    Temporary change in MainPage

--- src/views/MainPage/MainPage.jsx
+++ src/views/MainPage/MainPage.jsx
@@ -6,15 +6,18 @@ import TeamSidebar from "../../components/TeamSidebar/TeamSidebar"
 
 const MainPage = () => {
 
-  const{teamId,dmId} = useParams()
-  
+  const { teamId, dmId } = useParams()
+
   return (
     <div className="font-sans antialiased h-screen flex overflow-x-hidden">
       <SideBar /> {/* Adjust width as needed */}
       <MyServers /> {/* Adjust width as needed */}
-      {teamId|| dmId ? <ChatBox /> : null} {/* Adjust width as needed */}
-      {teamId || dmId ? <TeamSidebar />:null  } {/* Adjust width as needed */}
+      <ChatBox /> {/* Adjust width as needed */}
+      <TeamSidebar />
     </div>
   )
 }
 export default MainPage;
+
+// {teamId|| dmId ? <ChatBox /> : null} {/* Adjust width as needed */}
+// {teamId || dmId ? <TeamSidebar />:null  } {/* Adjust width as needed *
