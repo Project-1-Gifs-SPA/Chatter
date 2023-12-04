abd775ecc0b912972ebb5befd46ebdfd110b9044
Aleksandar Spasov <alekspasov@gmail.com>
2023-12-04 13:21:14 +0200

    Added component state in Dashboard.jsx

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../../components/SearchBar/SearchBar'
 
 const Dashboard = () => {
+
+	const [show, setShow] = useState('online')
+
 	return (
 		<div className='bg-gray-700 w-full'>
 			<div className="bg-gray-700 w-[1000px] h-[100vh] py-4 mx-auto">
@@ -9,16 +12,23 @@ const Dashboard = () => {
 					<div className="flex items-center">
 						<p className="text-white mb-4 lg:mb-0 ml-4">Friends</p>
 						<div className="lg:ml-4">
-							<button className="btn btn-xs btn-neutral">Online</button>
-							<button className="btn btn-xs ml-4 btn-neutral">All</button>
-							<button className="btn btn-xs ml-4 btn-neutral">Friend Requests</button>
-							<button className="btn btn-xs ml-4 btn-neutral">Meetings</button>
+							<button className="btn btn-xs btn-neutral"onClick={()=>setShow('online')}>Online</button>
+							<button className="btn btn-xs ml-4 btn-neutral" onClick={()=>setShow('all')}>All</button>
+							<button className="btn btn-xs ml-4 btn-neutral" onClick={()=>setShow('requests')}>Friend Requests</button>
+							<button className="btn btn-xs ml-4 btn-neutral" onClick={()=>setShow('meetings')}>Meetings</button>
 						</div>
 					</div>
 					
 				</div>
 				<div className="lg:mt-0 w-[550px]">
 						<SearchBar />
+					</div>
+
+					<div>
+						{show==='online' ? <p>online friends</p>: null}
+						{show==='all'? <p> all friends</p>: null}
+						{show==='requests'? <p> friends request</p>: null}
+						{show==='meetings'? <p> meetings</p>: null}   
 					</div>
 			
 			</div>
