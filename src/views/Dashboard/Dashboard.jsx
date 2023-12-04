04a3bf4c6bf2f35e7764e22fd42c3beb5a86cdb3
banagreg <bogdanovagergana97@gmail.com>
2023-12-04 11:31:10 +0200

    Added initial style to Dashboard

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import SearchBar from '../../components/SearchBar/SearchBar'
 
 const Dashboard = () => {
 	return (
-		<div className="bg-gray-900 w-full h-100vh">
-			<p className='text-white'>Dashboard</p>
+		<div className='bg-gray-700 w-full'>
+			<div className="bg-gray-700 w-[1000px] h-[100vh] py-4 mx-auto">
+				<div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
+					<div className="flex items-center">
+						<p className="text-white mb-4 lg:mb-0 ml-4">Friends</p>
+						<div className="lg:ml-4">
+							<button className="btn btn-xs btn-neutral">Online</button>
+							<button className="btn btn-xs ml-4 btn-neutral">All</button>
+							<button className="btn btn-xs ml-4 btn-neutral">Friend Requests</button>
+							<button className="btn btn-xs ml-4 btn-neutral">Meetings</button>
+						</div>
+					</div>
+					<div className="lg:mt-0 w-[550px]">
+						<SearchBar />
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
