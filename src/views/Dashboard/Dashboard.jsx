9cec0bcf96ae78cb832393e8b72141c020ff0813
banagreg <bogdanovagergana97@gmail.com>
2023-12-04 13:33:15 +0200

    Changed style in Dashboard

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -4,21 +4,24 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 const Dashboard = () => {
 	return (
 		<div className='bg-gray-700 w-full'>
-			<div className="bg-gray-700 w-[1000px] h-[100vh] py-4 mx-auto">
+			<div className="bg-gray-800 w-[1100px] h-[100vh] px-4 mx-auto">
 				<div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
 					<div className="flex items-center">
 						<p className="text-white mb-4 lg:mb-0 ml-4">Friends</p>
-						<div className="lg:ml-4">
+						<div className="lg:ml-4 flex items-center">
 							<button className="btn btn-xs btn-neutral">Online</button>
 							<button className="btn btn-xs ml-4 btn-neutral">All</button>
 							<button className="btn btn-xs ml-4 btn-neutral">Friend Requests</button>
+							<div className='ml-4 divider h-[25px] border-l border-gray-500'></div>
 							<button className="btn btn-xs ml-4 btn-neutral">Meetings</button>
 						</div>
 					</div>
-					<div className="lg:mt-0 w-[550px]">
+					<div className="lg:mt-0 mb-4">
 						<SearchBar />
 					</div>
 				</div>
+				<div className='divider'></div>
+
 			</div>
 		</div>
 	)
